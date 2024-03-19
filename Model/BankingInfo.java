cff12c9b1ce896c9d8c9ae052d83fd8ed71b21d3
hoangdv <duongviethoang2042002@gmail.com>
2024-03-19 16:20:50 +0700

    Create first sample data

---
+++ Model/BankingInfo.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class BankingInfo {
+    private String bank;
+    private String name;
+    private String number;
+
+    public BankingInfo(String bank, String name, String number) {
+        this.bank = bank;
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
